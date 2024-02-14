d8eee9ddd3804917c4bfa9f035425f63f661780a
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2024-02-14 23:12:09 +0530

    Create New Random Phone Number Generate App

---
+++ android/app/src/main/kotlin/com/example/themp01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.themp01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import './screens/fstscreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MaterialApp',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: const MyHomePage(
+          key: Key('HomePage'), title: 'Random Phone Number Generator'),
+    );
+  }
+}
